be5dab2722d3dd868d3f9f82dc23e373e7349794
viv-iam <vivian.opondoh@gmail.com>
2017-07-28 18:41:41 +0300

    Edit src/index.html

---
+++ src/app/mock-patients.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, fname: 'Dunday', lname:'Mony'},
+  { id: 2, fname: 'Neville',  lname: 'Tp'},
+  { id: 3, fname: 'Brew',  lname: 'Gron'},
+  { id: 4, fname: 'Calva', lname: 'Sly' },
+  { id: 5, fname: 'Twin', lname:'Osogo' },
+  { id: 6, fname: 'Ruby', lname: 'Jay' },
+  { id: 7, fname: 'Dino', lname: 'Jae'},
+  { id: 8, fname: 'Minny', lname: 'Mel'},
+  { id: 9, fname: 'Ginger', lname: 'Spy'},
+  { id: 0, fname: 'Tandy', lname: 'Fi'}
+];
