704741ed0f9818734e6413470430a2df52b52ee7
viv-iam <vivian.opondoh@gmail.com>
2017-07-28 17:16:48 +0300

    Import PatientDetail, PatientsComponent, PatientService in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule }       from '@angular/core';
 import { FormsModule }    from '@angular/forms';
+import { BrowserModule }  from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { PatientDetailsComponent }  from './hero-detail.component';
+import { PatientsComponent }      from './heroes.component';
+import { PatientService }          from './hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
