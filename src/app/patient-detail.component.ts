8746148762067eb60b3915cafc1b324a894fb948
viv-iam <vivian.opondoh@gmail.com>
2017-07-28 17:52:38 +0300

    Import all relevant components in patient-detail.component.ts

---
+++ src/app/patient-detail.component.ts
@@ -0,0 +1,7 @@
+import 'rxjs/add/operator/switchMap';
+import { Component, OnInit }        from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location }                 from '@angular/common';
+
+import { Patient }         from './patient';
+import { PatientService }  from './patient.service';
