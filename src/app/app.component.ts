f55914ccc6facead3e2d75538c810c614c9d8c1a
viv-iam <vivian.opondoh@gmail.com>
2017-07-28 17:06:51 +0300

    Add @Component decorator to app.components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   template: `
     <h1>{{title}}</h1>
     <nav>
-      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+      <a routerLink="/dashboard" routerLinkActive="active">Home</a>
+      <a routerLink="/patients" routerLinkActive="active">Patients</a>
     </nav>
     <router-outlet></router-outlet>
   `,
