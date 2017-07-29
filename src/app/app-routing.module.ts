b13d95ba288f13e53d9a9803ee24e23dd30dd9af
viv-iam <vivian.opondoh@gmail.com>
2017-07-29 09:30:08 +0300

    Adjust patient.component.html make patient name clickable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardComponent }   from './dashboard.component';
 import { PatientsComponent }      from './patients.component';
 import { PatientDetailComponent }  from './patient-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/patients', pathMatch: 'full' },
   { path: 'patients',     component: PatientsComponent },
-  { path: 'dashboard',  component: DashboardComponent },
   { path: 'detail/:id', component: PatientDetailComponent }
 ];
 
--- src/app/patient.service.ts
+++ src/app/patient.service.ts
@@ -1,18 +1,13 @@
+import { Injectable } from '@angular/core';
+
 import { Patient } from './patient';
 import { PATIENTS } from './mock-patients';
-import { Injectable } from '@angular/core';
+
 
 @Injectable()
 export class PatientService {
   getPatients(): Promise<Patient[]> {
     return Promise.resolve(PATIENTS);
-  }
-
-  getPatientsSlowly(): Promise<Patient[]> {
-    return new Promise(resolve => {
-
-      setTimeout(() => resolve(this.getPatients()), 2000);
-    });
   }
 
   getPatient(id: number): Promise<Patient> {
